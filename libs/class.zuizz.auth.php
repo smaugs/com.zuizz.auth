854402b7ce6d92ed31f8a9aa312200a247ed728b
Veith Zäch <veithz@gmail.com>
2013-04-09 10:14:34 +0200

    nonexistent user without 500 error

--- libs/class.zuizz.auth.php
+++ libs/class.zuizz.auth.php
@@ -1,70 +1,87 @@
 <?php
-ZU::load_interface('zuizz.auth','auth');
-class ZUAUTH implements ZUAUTH_interface{
-	var $is_authenticated = FALSE;
-	var $auth_failure = FALSE;
-/*
- * Die ZUAUTH classe ist eine Implementierung des Auth interfaces
- * Hier wird die Authentification erledigt, die rechte zum entsprechenden login werden in ./class.zuizz.permissions.php erledigt
- *
- */
-	function __construct(){
+ZU::load_interface('zuizz.auth', 'auth');
+class ZUAUTH implements ZUAUTH_interface
+{
+    var $is_authenticated = FALSE;
+    var $auth_failure = FALSE;
 
-	}
+    /*
+     * Die ZUAUTH classe ist eine Implementierung des Auth interfaces
+     * Hier wird die Authentification erledigt, die rechte zum entsprechenden login werden in ./class.zuizz.permissions.php erledigt
+     *
+     */
+    function __construct()
+    {
 
-	public function login($username, $credentials) {
-		if (isset ( $username ) && isset ($credentials) && !empty($username) ) {
-            $tmp = ORM::for_table('org_user')->where('name', $username)->find_one()->as_array();
+    }
 
-			if ($username == $tmp['name'] && $tmp['password'] == md5 ( $GLOBALS ['ZUIZZ']->config->system ['salt'] . $credentials . $GLOBALS ['ZUIZZ']->config->system ['salt'] )) {
-				$_SESSION ['ZUIZZ'] ['AUTH'] ['is_auth'] = TRUE;
-				$_SESSION ['ZUIZZ'] ['AUTH'] ['username'] = $username;
-				$_SESSION ['ZUIZZ'] ['AUTH'] ['uid'] = $tmp['user_id'];
-				$_SESSION ['ZUIZZ'] ['AUTH'] ['contact'] = $tmp['contact_id'];
-				$_SESSION ['ZUIZZ'] ['AUTH'] ['org'] = $tmp['org_id'];
-				$this->is_authenticated = TRUE;
-				// Permission reinitialisieren
-				$GLOBALS['ZUIZZ']->perm->init ();
-				return true;
-			} else {
-				$this->auth_failure = TRUE;
-				unset ( $_SESSION ['ZUIZZ'] ['AUTH'] );
-				unset ( $_SESSION ['ZUIZZ'] ['PERM'] );
-				return false;
-			}
+    public function login($username, $credentials)
+    {
+        if (isset ($username) && isset ($credentials) && !empty($username)) {
+            $tmp = ORM::for_table('org_user')->where('name', $username)->find_one();
+            if ($tmp) {
+                $d = $tmp->as_array();
 
-		} else {
-			unset ( $_SESSION ['ZUIZZ'] ['AUTH'] );
-			unset ( $_SESSION ['ZUIZZ'] ['PERM'] );
-			return false;
-		}
+                if ($username == $d['name'] && $d['password'] == md5($GLOBALS ['ZUIZZ']->config->system ['salt'] . $credentials . $GLOBALS ['ZUIZZ']->config->system ['salt'])) {
+                    $_SESSION ['ZUIZZ'] ['AUTH'] ['is_auth'] = TRUE;
+                    $_SESSION ['ZUIZZ'] ['AUTH'] ['username'] = $username;
+                    $_SESSION ['ZUIZZ'] ['AUTH'] ['uid'] = $d['user_id'];
+                    $_SESSION ['ZUIZZ'] ['AUTH'] ['contact'] = $d['contact_id'];
+                    $_SESSION ['ZUIZZ'] ['AUTH'] ['org'] = $d['org_id'];
+                    $this->is_authenticated = TRUE;
+                    // Permission reinitialisieren
+                    $GLOBALS['ZUIZZ']->perm->init();
+                    return true;
+                } else {
+                    $this->auth_failure = TRUE;
+                    unset ($_SESSION ['ZUIZZ'] ['AUTH']);
+                    unset ($_SESSION ['ZUIZZ'] ['PERM']);
+                    return false;
+                }
+            } else {
+                $this->auth_failure = TRUE;
+                unset ($_SESSION ['ZUIZZ'] ['AUTH']);
+                unset ($_SESSION ['ZUIZZ'] ['PERM']);
+                return false;
+            }
 
-	}
+        } else {
+            unset ($_SESSION ['ZUIZZ'] ['AUTH']);
+            unset ($_SESSION ['ZUIZZ'] ['PERM']);
+            return false;
+        }
+
+    }
 
 
-	// on NULL return user_id of logged in user
-    public function get_user_id($username = NULL){
-	    if(isset($username)){
-		    // TODO user_id des entsprechenden users zurückgeben wenn bestimmter username verlangt wird
-	    }else {
-	    	$_SESSION ['ZUIZZ'] ['AUTH'] ['uid'];
-	    }
+    // on NULL return user_id of logged in user
+    public function get_user_id($username = NULL)
+    {
+        if (isset($username)) {
+            // TODO user_id des entsprechenden users zurückgeben wenn bestimmter username verlangt wird
+        } else {
+            $_SESSION ['ZUIZZ'] ['AUTH'] ['uid'];
+        }
     }
 
     // on NULL return user_name of logged in user
-    public function get_user_name($user_id = NULL){
-    if(isset($user_id)){
-		    // TODO get_user_name des entsprechenden users zurückgeben wenn bestimmter username verlangt wird
-	    }else {
-	    	$_SESSION ['ZUIZZ'] ['AUTH'] ['username'];
-	    }
+    public function get_user_name($user_id = NULL)
+    {
+        if (isset($user_id)) {
+            // TODO get_user_name des entsprechenden users zurückgeben wenn bestimmter username verlangt wird
+        } else {
+            $_SESSION ['ZUIZZ'] ['AUTH'] ['username'];
+        }
     }
 
     // Logout
-    public function logout(){
-    	// rechte zurücksetzen
-    	unset ( $_SESSION ['ZUIZZ'] );
+    public function logout()
+    {
+        // rechte zurücksetzen
+        unset ($_SESSION ['ZUIZZ']);
         session_destroy();
     }
 
-};
+}
+
+;
